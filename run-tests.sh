76397359a6b767bfbf10a44f69bda77d784f80aa
Antoine Labour <piman@chromium.org>
2014-06-02 06:47:09 -0700

    Pass extra command-line flags to testit_android

--- run-tests.sh
+++ run-tests.sh
@@ -9,4 +9,4 @@ if [ ${TOOLCHAIN} = "clang++" ]; then
   EXTRA_FLAGS=--with-compiler-rt
 fi
 
-PATH="${INSTALL_DIR}/bin:${PATH}" "${TESTIT_DIR}/testit_android" --abi=armeabi-v7a --verbose --verbose --verbose --cxx=arm-linux-androideabi-${TOOLCHAIN} ${EXTRA_FLAGS}
+PATH="${INSTALL_DIR}/bin:${PATH}" "${TESTIT_DIR}/testit_android" --abi=armeabi-v7a --verbose --verbose --verbose --cxx=arm-linux-androideabi-${TOOLCHAIN} ${EXTRA_FLAGS} "$@"
